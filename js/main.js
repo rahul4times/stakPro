65ec83077450fc926eff4c97fdd338c6d9be9488
rahul4times <ams@Rahuls-MacBook-Air.local>
2017-09-20 13:39:23 -0700

    Added FOR LOOP to pull data from API

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
   var searchBtn = document.getElementById('searchBtn');
   var imgDiv = document.getElementById('companyLogo');
   var companyProfileDiv = document.getElementById('company-profile');
-  var detailBoxDiv = document.getElementById('details');
+  var quoteDetailDiv = document.getElementById('quote-details');
   var logoDiv = document.getElementById('companyLogo');
   var currentPriceDiv = document.getElementById('currentPrice');
+  var statsDiv = document.getElementById('stats');
+
 
   // API request starts here
   var link = "https://api.iextrading.com/1.0/stock/";
@@ -27,9 +29,12 @@ $(document).ready(function(){
 
   searchBtn.addEventListener('click',function(){
 
+    //onClick scrolling page to result
+    window.scrollBy(0, 700);
+
     // API call for logo starts here
     $.get(link + symbol.value + logo, function(data){
-
+      $(imgDiv).empty();
       var img = document.createElement('img');
       img.src = data.url;
       imgDiv.appendChild(img);
@@ -42,8 +47,7 @@ $(document).ready(function(){
     setInterval(function(){
       // Current Price API call starts here
       $.get(link + symbol.value + price, function(currentPriceValue){
-
-        //pricePara.innerHTML = 0;
+        $(currentPriceDiv).empty();
         pricePara.innerHTML = '$' + currentPriceValue;
         currentPriceDiv.appendChild(pricePara);
         //console.log(pricePara);
@@ -52,54 +56,130 @@ $(document).ready(function(){
     }, 1000);
     // setInterval function ends here
 
+    // ----------------------------------------------------------------//
 
+    // Labels for Quote
+    var quoteDetailSymbol = ['Symbol:','Primary Exchange:','Calculation Price:','Real Time Price:','Real Time Size:','Last Updated:','Delayed Price:','Delayed Price Time:','Previous Close:','Change:',
+      'Change Percent:','Market Percent:','Volume:','Average Total Volume:','Bid Price:','Bid Size:','Ask Price:','Ask Size:','Market Capital:','52 Week High:','52 Week Low:'];
 
+    // Empty array for quote value
+    var quoteDetailvalue = [];
 
-
-    // API call for company Info starts here
-    var para = document.createElement('p');
-    $.get(link + symbol.value + company, function(companyInfo){
-
-
-      para.innerHTML = 'Symbol: ' + companyInfo.symbol + '<br>' +
-      'Company Name: ' + companyInfo.companyName + '<br>' +
-      'Exchange: ' + companyInfo.exchange + '<br>' +
-      'Industry: ' + companyInfo.industry + '<br>' +
-      'Website: ' + companyInfo.website + '<br>' +
-      'Description: ' + companyInfo.description + '<br>' +
-      'CEO: ' + companyInfo.CEO;
-      companyProfileDiv.appendChild(para);
-    }); // API call for company info
+    // Creating table
+    var quoteDetailTable = document.createElement('table');
 
     // API call for Detail Box starts here
     var detailPara = document.createElement('p');
-    $.get(link + symbol.value + quote, function(companyDetails){
+    $.get(link + symbol.value + quote, function(quoteInfo){
 
+      $(quoteDetailDiv).empty();
 
-      detailPara.innerHTML = 'Symbol: ' + companyDetails.symbol + '<br>' +
-        'Primary Exchange: ' + companyDetails.primaryExchange + '<br>' +
-        'Calculation Price: ' + companyDetails.calculationPrice + '<br>' +
-        'Real Time Price: ' + companyDetails.iexRealtimePrice + '<br>' +
-        'Real Time Size: ' + companyDetails.iexRealtimeSize + '<br>' +
-        'Last Updated: ' + companyDetails.iexLastUpdated + '<br>' +
-        'Delayed Price: ' + companyDetails.delayedPrice + '<br>' +
-        'Delayed Price Time : ' + companyDetails.delayedPriceTime + '<br>' +
-        'Previous Close: ' + companyDetails.previousClose + '<br>' +
-        'Change: ' + companyDetails.change + '<br>' +
-        'Change Percent: ' + companyDetails.changePercent + '<br>' +
-        'Market Percent: ' + companyDetails.iexMarketPercent + '<br>' +
-        'Volume: ' + companyDetails.iexVolume + '<br>' +
-        'Average Total Volume: ' + companyDetails.avgTotalVolume + '<br>' +
-        'Bid Price: ' + companyDetails.iexBidPrice + '<br>' +
-        'Bid Size: ' + companyDetails.iexBidSize + '<br>' +
-        'Ask Price: ' + companyDetails.iexAskPrice + '<br>' +
-        'Ask Size: ' + companyDetails.iexAskSize + '<br>' +
-        'Market Capital: ' + companyDetails.marketCap + '<br>' +
-        '52 Week High: ' + companyDetails.week52High + '<br>' +
-        '52 Week Low: ' + companyDetails.week52Low;
-      detailBoxDiv.appendChild(detailPara);
+      // Pushing api value in quoteDetailvalue
+      for(let key in quoteInfo){
+        quoteDetailvalue.push(quoteInfo[key]);
+      }
+
+      // Creating table tr and td to display data
+
+      for(let i=0; i<quoteDetailSymbol.length; i++){
+
+        var quoteDetailRow = document.createElement('tr');
+        var quoteDetailTdSymbol = document.createElement('td');
+        quoteDetailTdSymbol.className = 'symbolColumn';
+
+        var quoteDetailTdValue = document.createElement('td');
+        quoteDetailTdValue.className = 'valueColumn';
+
+        quoteDetailTdSymbol.innerHTML = quoteDetailSymbol[i];
+        quoteDetailTdValue.innerHTML = quoteDetailvalue[i];
+        quoteDetailRow.appendChild(quoteDetailTdSymbol);
+        quoteDetailRow.appendChild(quoteDetailTdValue);
+        quoteDetailTable.appendChild(quoteDetailRow);
+      }
+
+      quoteDetailDiv.appendChild(quoteDetailTable);
+
 
     }); // API call for Detail Box ends here
+
+    // ----------------------------------------------------------------//
+
+    // API call for Company starts here
+    var companyProfileTable = document.createElement('table');
+    // Label for company tab
+    var companyProfileLabel = ['Symbol:','Company Name:','Exchange:',
+      'Industry:','Website:','Description:','CEO:'];
+    // Data from API will go in this empty array
+    var companyProfileData = [];
+
+
+    $.get(link + symbol.value + company, function(companyInfo){
+
+      $(companyProfileDiv).empty();
+
+      // Data from API goes in companyProfileData
+      for(let key in companyInfo){
+        companyProfileData.push(companyInfo[key]);
+      }
+
+
+        for(let i=0; i<companyProfileLabel.length; i++){
+
+          var companyProfileTableRow = document.createElement('tr');
+          var companyProfileTableDataSymbol = document.createElement('td');
+          companyProfileTableDataSymbol.className = 'symbolColumn';
+
+          var companyProfileTableDataValue = document.createElement('td');
+          companyProfileTableDataValue.className = 'valueColumn';
+
+          companyProfileTableDataSymbol.innerHTML = companyProfileLabel[i];
+          companyProfileTableDataValue.innerHTML = companyProfileData[i];
+          companyProfileTableRow.appendChild(companyProfileTableDataSymbol);
+          companyProfileTableRow.appendChild(companyProfileTableDataValue);
+          companyProfileTable.appendChild(companyProfileTableRow);
+
+        }
+        companyProfileDiv.appendChild(companyProfileTable);
+    }); // API call for Company ends here
+
+
+
+    // Key stats api call starts here
+    var statsLabel = ['Company Name: ','Market Capital: ','Beta: ','Week 52 High:','Week 52 Low:','Week 52 Change:','Short Interest:','Short Date:','Dividend Rate:','Dividend Yield:','Ex Dividend Date:','Latest EPS:','Latest EPS Date:','Shares Outstanding:','Float:','Return On Equity:','Consensus EPS:','Number Of Estimates:','Symbol:','EBITDA:',
+    'Revenue:','Gross Profit:','Cash:','Debt:','Trailing 12 Mon EPS:','Revenue Per Share:','Revenue Per Employee:','PE Ratio High:', 'PE Ratio Low:'];
+
+    // Empty array
+    var statsData = [];
+
+    // Creating table for statsInfo
+    var statsTable = document.createElement('table');
+
+    $.get(link + symbol.value + keyStats, function(statsInfo){
+
+      // Pushing in statsData from API
+      for(let key in statsInfo){
+        statsData.push(statsInfo[key]);
+      }
+
+      // Displaying data
+
+      for(let i=0; i<statsLabel.length; i++){
+        var statsRow = document.createElement('tr');
+        var statsTdSymbol = document.createElement('td');
+        statsTdSymbol.className = 'symbolColumn';
+        var statsTdValue = document.createElement('td');
+        statsTdValue.className = 'valueColumn';
+
+        statsTdSymbol.innerHTML = statsLabel[i];
+        statsTdValue.innerHTML = statsData[i];
+        statsRow.appendChild(statsTdSymbol);
+        statsRow.appendChild(statsTdValue);
+        statsTable.appendChild(statsRow);
+      }
+      statsDiv.appendChild(statsTable);
+
+
+    });
 
 
 
